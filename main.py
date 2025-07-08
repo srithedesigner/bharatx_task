1723b01c232d6aaf578e3477cf6aa6d9a7c51785
srithedesigner <srivaishnavsri@gmail.com>
2025-07-09 00:33:18 +0530

    added frontend in trusted

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from core import get_sorted_prices
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://srithedesigner.github.io"],
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"], 
+)
 
 class PriceRequest(BaseModel):
     country: str
